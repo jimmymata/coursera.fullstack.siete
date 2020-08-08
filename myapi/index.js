ba5b55daebb4c6f65a8c4ef6282eaa238310a192
Jimmy Mata <jimmymata@gmail.com>
2020-08-08 16:36:16 -0600

    add express server

---
+++ myapi/index.js
@@ -0,0 +1,35 @@
+// var declaration
+data = ['a','b','c']
+
+// const declaration
+const cors    = require('cors')
+const express = require('express')
+const port    = 3000
+
+// app declaration
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// start app
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`)
+})
+
+// home page
+app.get('/', (req, res) => {
+  res.send('Express server running !!!')
+})
+
+// show saved data
+app.get('/getdata', (req, res) => {
+  res.json(data)
+})
+
+// add new data
+app.get('/postdata', (req, res) => {
+  data.push(req.data)
+  res.json(data)
+})
+
